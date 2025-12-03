fafab76f0c9cd8e2056d3b5a62684c48e121367e
Will Hulme <25637345+wjchulme@users.noreply.github.com>
2025-12-03 17:19:55 +0000

    fix agebandfilter bug

--- analysis/0-lib/design.R
+++ analysis/0-lib/design.R
@@ -369,7 +369,7 @@ level_combos <- expand_grid(group1 = level1_group, group2 = level2_group) |>
   ) |>
   filter(
     (group1 == group2) %in% c(FALSE, NA) | (group1 == "all"),
-    !(group1 == ageband4 & group2 == ageand13)
+    !(group1 == "ageband4" & group2 == "ageand13")
   )
 
 # Local run flag ----
